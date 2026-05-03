00fdc9ca2f26ce4f4771f04f71b946345f5fa3de
vignesh <dinovicky1010@gmail.com>
2026-05-03 12:48:59 +0530

    Port updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.MONGO_URL || 3000;
+const PORT = process.env.PORT || 3000;
 
 // ✅ MongoDB Connection
 mongoose
