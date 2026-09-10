eb19bba55f5ce449603d6023110ae7ffa6d1f1ed
Vignesh <148333939+Vjviki@users.noreply.github.com>
2026-09-10 12:18:57 +0530

    Register detected transaction Capacitor plugin

--- my-vite-app/android/app/src/main/java/com/vjviki/moneymanager/MainActivity.java
+++ my-vite-app/android/app/src/main/java/com/vjviki/moneymanager/MainActivity.java
@@ -1,5 +1,13 @@
 package com.vjviki.moneymanager;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(DetectedTransactionPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
